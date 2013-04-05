6938e8a72267f4416e3fc2cbcb6e76a817f81312
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-04-04 21:09:24 -0300

    final do dojo

--- lib/dojo-4.rb
+++ lib/dojo-4.rb
@@ -1,3 +1,27 @@
-def teste
-	true
+def fatores_primos( numero )
+	fator_resp = []
+
+	numero_encontrado = 2
+	while numero != 1
+		numero_encontrado = encontrar_fator(numero_encontrado,numero)
+		numero /= numero_encontrado
+		fator_resp << numero_encontrado
+	end
+	fator_resp
 end
+
+def encontrar_fator(inicio,numero)
+	(inicio..numero).find do |i|
+		divisivel?(numero, i)
+	end
+end
+
+def divisivel?(dividendo, divisor)
+	string = 'a' * dividendo
+
+	/\A(a{#{divisor}})+\z/ =~ string
+end
+
+# def divisivel?(dividendo, divisor)
+# 	dividendo % divisor == 0
+# end
--- spec/dojo-4_spec.rb
+++ spec/dojo-4_spec.rb
@@ -1,7 +1,40 @@
 require 'spec_helper'
 
 describe "dojo-4" do
-	it "return true " do
-		teste.should == true
+
+	subject { fatores_primos(numero) }
+	context "fatores primos do 1" do
+		let(:numero) { 1 }
+		it { should == [] }
+	end
+
+	describe "fatores primos do 2" do
+		let(:numero) { 2 }
+		it { should == [2] }
+	end
+
+	describe "fatores primos do 3" do
+		let(:numero) { 3 }
+		it { should == [3] }
+	end
+
+	describe "fatores primos do 4" do
+		let(:numero) { 4 }
+		it { should == [2,2] }
+	end
+
+	describe "fatores primos do 5" do
+		let(:numero) { 5 }
+		it { should == [5] }
+	end
+
+	describe "fatores primos do 6" do
+		let(:numero) { 6 }
+		it { should == [2,3] }
+	end
+
+	describe "fatores primos do 276" do
+		let(:numero) { 276 }
+		it { should == [2, 2, 3, 23] }
 	end
 end
